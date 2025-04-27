79a5a689d9d1ba3c199b72b13ca59458e9d365ee
Angelika V <angelikavinorov@gmail.com>
2025-04-27 18:20:40 -0500

    Added Prework files to repository

---
+++ ButtonApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ButtonApp
+//
+//  Created by Angelika ❤️ on 4/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorButton: UIButton! // this is change background button
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+           view.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor{
+
+           let red = CGFloat.random(in: 0...1)
+           let green = CGFloat.random(in: 0...1)
+           let blue = CGFloat.random(in: 0...1)
+
+           return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+       }
+    
+
+}
+
---
+++ ButtonAppTests/ButtonAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonAppTests.swift
+//  ButtonAppTests
+//
+//  Created by Angelika ❤️ on 4/23/25.
+//
+
+import Testing
+@testable import ButtonApp
+
+struct ButtonAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
